adad29e92f79a99cdbade962eee3298f5f0293c5
Matthew Hall <thematthewhall7@gmail.com>
2024-11-18 15:06:32 -0500

    Create Item.java

---
+++ Item.java
@@ -0,0 +1,30 @@
+public class Item {
+    private String type;
+    private String crust;
+    private String size;
+    private boolean[] toppings;
+    private int amount;
+    Item(String crust, String size, boolean[] toppings, int amount) {
+        type = "pizza";
+        this.crust = crust;
+        this.size = size;
+        this.toppings = toppings;
+        this.amount = amount;
+    }
+
+    Item(String side, int amount) {
+        type = "side";
+        this.amount = amount;
+    }
+
+    Item(String drink, String size, int amount) {
+        type = "drink";
+        this.size = size;
+        this.amount = amount;
+    }
+
+    Item(int amount) {
+        type = "dessert";
+        this.amount = amount;
+    }
+}
