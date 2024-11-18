07bfbda7963f5facf3e600d072c07ef43038e83f
Matthew Hall <thematthewhall7@gmail.com>
2024-11-18 16:51:26 -0500

    Update Item.java

--- Item.java
+++ Item.java
@@ -7,11 +7,64 @@ public class Item {
     private float price;
     Item(String crust, String size, boolean[] toppings, int amount) {
         type = "pizza";
-        price = 0;
         this.crust = crust;
         this.size = size;
         this.toppings = toppings;
         this.amount = amount;
+
+        // Calculates the price
+        price = 0;
+        boolean atLeastOne = false;
+        switch (size) {
+            case "small" -> {
+                price += 5;
+                for(int i=1; i<toppings.length; i++) {
+                    if(toppings[i]) {
+                        atLeastOne = true;
+                        price += 0.75f;
+                    }
+                }
+                if(atLeastOne) {
+                    price -= 0.75f;
+                }
+            }
+            case "medium" -> {
+                price += 7;
+                for(int i=1; i<toppings.length; i++) {
+                    if(toppings[i]) {
+                        atLeastOne = true;
+                        price += 1f;
+                    }
+                }
+                if(atLeastOne) {
+                    price -= 1f;
+                }
+            }
+            case "large" -> {
+                price += 9;
+                for(int i=1; i<toppings.length; i++) {
+                    if(toppings[i]) {
+                        atLeastOne = true;
+                        price += 1.25f;
+                    }
+                }
+                if(atLeastOne) {
+                    price -= 1.25f;
+                }
+            }
+            default -> {
+                price += 11;
+                for(int i=1; i<toppings.length; i++) {
+                    if(toppings[i]) {
+                        atLeastOne = true;
+                        price += 1.50f;
+                    }
+                }
+                if(atLeastOne) {
+                    price -= 1.50f;
+                }
+            }
+        }
     }
 
     Item(String side, int amount) {
@@ -28,5 +81,80 @@ public class Item {
     Item(int amount) {
         type = "dessert";
         this.amount = amount;
+    }
+
+    @Override
+    public String toString() {
+        if(type.equals("pizza")) {
+            return "<html>"+size+" "+crust+" pizza<br/>"+toppingsToText(size, toppings)+"<html>";
+
+        } else if(type.equals("side")) {
+
+        } else if(type.equals("drink")) {
+
+        } else {
+            return "hi";
+        }
+        return null;
+    }
+
+    private String toppingsToText(String size, boolean[] toppings) {
+        String text = "";
+        switch (size) {
+            case "small" -> {
+                //
+            }
+            case "medium" -> {
+                //
+            }
+            case "large" -> {
+                //
+            }
+            default-> {
+                //
+            }
+        }
+        boolean atLeastOne = false;
+        for(int i=0; i<toppings.length; i++) {
+            if((toppings[i]) && i==0){
+                text += "<html>Cheese           $0.00<br/><html>";
+            }
+            if((toppings[i]) && i==1){
+                atLeastOne = true;
+                if(atLeastOne) {
+                    
+                }
+                text += "<html>Ham           $0.00<br/><html>";
+            }
+            if((toppings[i]) && i==2){
+                atLeastOne = true;
+                text += "<html>Tomato<br/><html>";
+            }
+            if((toppings[i]) && i==3){
+                atLeastOne = true;
+                text += "<html>Pepperoni<br/><html>";
+            }
+            if((toppings[i]) && i==4){
+                atLeastOne = true;
+                text += "<html>Green Pepper<br/><html>";
+            }
+            if((toppings[i]) && i==5){
+                atLeastOne = true;
+                text += "<html>Mushroom<br/><html>";
+            }
+            if((toppings[i]) && i==6){
+                atLeastOne = true;
+                text += "<html>Sausage<br/><html>";
+            }
+            if((toppings[i]) && i==7){
+                atLeastOne = true;
+                text += "<html>Onion<br/><html>";
+            }
+            if((toppings[i]) && i==8){
+                atLeastOne = true;
+                text += "<html>Pineapple<br/><html>";
+            }
+        }
+        return text;
     }
 }
